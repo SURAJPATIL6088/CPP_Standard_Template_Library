e62fac9f59e52a40e8df00a868b6e3788ef023ca
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2024-01-16 17:13:38 +0530

    380. Insert Delete GetRandom O(1).cpp

--- Sets_And_Unordered_sets/Unordered Set/380. Insert Delete GetRandom O(1).cpp
+++ Sets_And_Unordered_sets/Unordered Set/380. Insert Delete GetRandom O(1).cpp
@@ -1,3 +1,16 @@
+/*
+Implement the RandomizedSet class:
+
+1. RandomizedSet() Initializes the RandomizedSet object.
+2. bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+3. bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+4. int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+
+Note : You must implement the functions of the class such that each function works in average O(1) time complexity.
+*/
+
+// https://leetcode.com/problems/insert-delete-getrandom-o1/
+
 class RandomizedSet {
 public:
     unordered_set<int>ust;
